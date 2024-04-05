260a543bb1140e586989ee95d8f8bb004f324b74
Saimon <saimon22011@gmail.com>
2024-04-05 12:39:47 +0400

    feat:reviews layout and its adaptive version

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let slides = document.querySelectorAll('.reviews .flex .slides-container .slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}

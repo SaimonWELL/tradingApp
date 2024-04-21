feb5f40aff2be52065d2dd7651a5e1b7b70eda70
Saimon <saimon22011@gmail.com>
2024-04-21 22:06:59 +0400

    feat:pricing layout and its adaptive version

--- js/script.js
+++ js/script.js
@@ -12,3 +12,24 @@ function prev(){
     index = (index - 1 + slides.length) % slides.length;
     slides[index].classList.add('active');
 }
+
+let monthBtn = document.querySelector(' .pricing .toggle-buttons .month-btn');
+let yearBtn = document.querySelector(' .pricing .toggle-buttons .year-btn');
+let monthlyPlan = document.querySelectorAll('.pricing .box-container .box .month');
+let yearlyPlan = document.querySelectorAll('.pricing .box-container .box .year');
+
+yearBtn.onclick = () =>{
+    monthBtn.classList.remove('active');
+    yearBtn.classList.add('active');
+
+    monthlyPlan.forEach(mo =>{ mo.style.display = 'none'});
+    yearlyPlan.forEach(yr =>{ yr.style.display = 'block'})
+}
+
+monthBtn.onclick = () =>{
+    monthBtn.classList.add('active');
+    yearBtn.classList.remove('active');
+
+    monthlyPlan.forEach(mo =>{ mo.style.display = 'block'});
+    yearlyPlan.forEach(yr =>{ yr.style.display = 'none'})
+}
